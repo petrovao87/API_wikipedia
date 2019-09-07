359be7eac27c86ef66a8a4c1bd8bd84e3714391e
petrovao87 <petrovao87@mail.ru>
2019-09-07 17:39:09 +0300

    add authentication

--- wiki/views.py
+++ wiki/views.py
@@ -2,21 +2,27 @@ from django.shortcuts import render
 from rest_framework import generics
 from wiki.serializers import NewNote, NoteList
 from wiki.models import Note
+import wikipedia
 
+from wiki.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
 class CreateNoteView(generics.CreateAPIView):
+
     serializer_class = NewNote
 
 
 class AllNoteView(generics.ListAPIView):
     serializer_class = NoteList
     queryset = Note.objects.all()
+    permission_classes = (IsAuthenticated, )
 
 
 class NoteView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = NewNote
     queryset = Note.objects.all()
+    permission_classes = (IsOwnerOrReadOnly, )
 
 

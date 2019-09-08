426c1e2395c2ac1580f61d0c9c4a7c6ee726b86d
petrovao87 <petrovao87@mail.ru>
2019-09-08 18:50:19 +0300

    change Create method in views

--- wiki/views.py
+++ wiki/views.py
@@ -18,7 +18,7 @@ class CreateNoteView(generics.CreateAPIView):
     #     return data
 
     serializer_class = NewNote
-    queryset = Note.objects.all()
+    # queryset = Note.objects.all()
 
     def create(self, request, *args, **kwargs):
         # print(request.data)
@@ -27,32 +27,37 @@ class CreateNoteView(generics.CreateAPIView):
         title = request.data['title']
         # print(url, title)
         try:
-            w = wikipedia.WikipediaPage(title)
+            w = wikipedia.WikipediaPage('title')
             content = w.content
             categories = w.categories
             links = w.links
             images = w.images
             # return self.create(request, *args, **kwargs)
             # data = json.dumps({'title': title, 'url': url, 'content': content, 'links': links, 'images': images})
-            user = self.get_queryset()
-            serializer = NewNote(data={'title': title, 'url': url, 'content': content, 'links': links, 'images': images})
+            # user = self.get_queryset()
+            serializer = NewNote(data={'title': title,
+                                       'url': url,
+                                       'content': content,
+                                       'categories': categories,
+                                       'links': links,
+                                       'images': images})
             # print(serializer)
 
             # serializer = NewNote(data='data')
             # print(data)
             # user = self.get_object()
             print(serializer.is_valid())
+            serializer.save()
             if serializer.is_valid():
                 print('_____'*100)
                 serializer.save()
             #     return Response('data')
             # return self.create(url, title, content, categories, links, images, *args, **kwargs)
-            return Response(data={'title': title, 'url': url, 'content': content, 'links': links, 'images': images},
+            return Response(data={'title': 'title', 'url': 'url', 'content': content, 'links': links, 'images': images},
                             status=status.HTTP_201_CREATED)
         except wikipedia.PageError:
             # serializer = NewNote(data=request.data)
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
 
 
 class AllNoteView(generics.ListAPIView):

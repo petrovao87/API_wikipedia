4e70d9ae9876874116312b4fb2229bcd6c1b29db
petrovao87 <petrovao87@mail.ru>
2019-09-08 14:57:15 +0300

    change Create method in views

--- wiki/views.py
+++ wiki/views.py
@@ -1,17 +1,44 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from wiki.serializers import NewNote, NoteList
 from wiki.models import Note
 import wikipedia
+import requests
+import json
 
 from wiki.permissions import IsOwnerOrReadOnly
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 # Create your views here.
 
 
 class CreateNoteView(generics.CreateAPIView):
+    # def post(self, request):
+    #     data = request.data
+    #     return data
 
     serializer_class = NewNote
+
+    def create(self, request, *args, **kwargs):
+        # print(request.data)
+        # print(request.query_params)
+        url = request.data['url']
+        title = request.data['title']
+        print(url, title)
+        try:
+            w = wikipedia.WikipediaPage(title)
+            content = w.content
+            categories = w.categories
+            links = w.links
+            images = w.images
+            # return self.create(request, *args, **kwargs)
+            data = json.dumps({'title': title, 'url': url, 'content': content, 'links': links, 'images': images}, )
+            print(data)
+            return self.create(url, title, content, categories, links, images, *args, **kwargs)
+        except wikipedia.PageError:
+            serializer = NewNote(data=request.data)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class AllNoteView(generics.ListAPIView):
@@ -23,6 +50,6 @@ class AllNoteView(generics.ListAPIView):
 class NoteView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = NewNote
     queryset = Note.objects.all()
-    permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticated)
 
 

d1af0d100002641a470dac289fcb323eff85fa09
petrovao87 <petrovao87@mail.ru>
2019-09-11 17:50:19 +0300

    debug = False

--- api_wikipedia/settings.py
+++ api_wikipedia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-8n^v!njwyi!fkajh^iv46tvu_#+y&iq&aj-jibv3n$6i03c%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
